f7aba19b35f2153b7dd3dca470d8da9e2a6dcc30
ronaldb123 <ronaldbeck@hotmail.co.uk>
2024-03-26 17:49:16 +0000

    ant design installed and frame scrolling added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,37 @@
-import { useEffect } from "react"
-import { getArtworkArr } from "../utility/api"
+import { useEffect, useState } from "react"
+import { getArtworks } from "../utility/api"
+import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons"
+import { Button } from "antd";
 
 
 export function HomePage() {
+    const [artworkData, setArtworkData] = useState([]); 
+    const [count, setCount] = useState(0);
 
+    const incrementCount = (increment) => {
+        if(increment === -1 && count === 0){
+
+        }else{
+            setCount((currentCount) => currentCount + increment);
+        }
+    }
+    
     useEffect(() => {
-        // getArtworkArr()
-        // .then((res) => {
-        //     console.log(res);
-        // })
-        // .catch((error) => {
-        //     console.log(error);
-        // })
+        getArtworks().then(({data}) => {
+            setArtworkData(data);
+        }).catch((err)=> console.log(err));
     },[])
-
+    
     return (
-        <>
-            <h1 className="">Welcome to the home page!</h1>
+        <div className="m-2">
+            <h1 className="text-center">Welcome to the home page!</h1>
         <div className="image-border mr-auto ml-auto">
-            <img src="https://i.ebayimg.com/images/g/iYMAAOSw269jXD2D/s-l1200.webp" className="w-full h-full max-w-100 max-h-100"></img>
+            <img src={artworkData[count] ? artworkData[count].images.web.url : <h1>Loading...</h1> } className="w-full h-full max-w-100 max-h-100"></img>
         </div>
-        </>
+        <div className="text-center m-5">
+            <Button icon={<LeftCircleOutlined className="text-xl"/>} onClick={()=> incrementCount(-1)}> </Button>
+            <Button icon={<RightCircleOutlined className="text-xl"/>} onClick={()=> incrementCount(1)}> </Button>
+        </div>
+        </div>
     )
 }
--- src/utility/api.js
+++ src/utility/api.js
@@ -1,31 +1,45 @@
 import axios from "axios";
 
-const metAPI = axios.create({
-    baseURL: "https://collectionapi.metmuseum.org/public/collection/v1",
+
+const clevAPI = axios.create({
+    baseURL: "https://openaccess-api.clevelandart.org/api/artworks",
 })
 
-export const getArtworkIDs = () => {
-    return metAPI
-    .get(`/search?hasImages=true&q=painting`)
+export const getArtworks = () => {
+    return clevAPI
+    .get(`?type=Painting&has_image=1`)
     .then(({data}) => {
-        if(data.objectIDs.length > 2){
-            return data.objectIDs.slice(data.objectIDs.length - 2);
-        }else{
-            return data.objectIDs;
-        }
+        return data;
     })
     .catch((err) => console.log(err));
 }
 
-export const getArtworkArr = () => {
-    return getArtworkIDs().then((data) => {
-        return Promise.all(
-        data.map(objectID =>{
-            return metAPI
-            .get(`/objects/${objectID}`)
-            .then(({data}) => {
-                return data;
-            })
-        }))
-    }) 
-}
+// const metAPI = axios.create({
+//     baseURL: "https://collectionapi.metmuseum.org/public/collection/v1",
+// })
+
+// export const getArtworkIDs = () => {
+//     return metAPI
+//     .get(`/search?hasImages=true&q=painting`)
+//     .then(({data}) => {
+//         if(data.objectIDs.length > 2){
+//             return data.objectIDs.slice(data.objectIDs.length - 2);
+//         }else{
+//             return data.objectIDs;
+//         }
+//     })
+//     .catch((err) => console.log(err));
+// }
+
+// export const getArtworkArr = () => {
+//     return getArtworkIDs().then((data) => {
+//         return Promise.all(
+//         data.map(objectID =>{
+//             return metAPI
+//             .get(`/objects/${objectID}`)
+//             .then(({data}) => {
+//                 return data;
+//             })
+//         }))
+//     }) 
+// }
