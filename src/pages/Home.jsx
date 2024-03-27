13b11a758574ca2720938fbc0e45e42f3e9b74c3
ronaldb123 <ronaldbeck@hotmail.co.uk>
2024-03-27 17:30:50 +0000

    Added ant popout menu with artwork info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
 import { getArtworks } from "../utility/api"
-import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons"
-import { Button, Image } from "antd";
+import { InfoCircleOutlined, LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons"
+import { Button, Divider, Drawer, Image, Space } from "antd";
 
 
 export function HomePage() {
     const [artworkData, setArtworkData] = useState([]); 
     const [count, setCount] = useState(0);
+    const [open, setOpen] = useState(false);
 
     const incrementCount = (increment) => {
         if(increment === -1 && count === 0){
@@ -15,7 +16,14 @@ export function HomePage() {
             setCount((currentCount) => currentCount + increment);
         }
     }
-
+    const showDrawer = () => {
+        setOpen(true);
+    }
+    
+    const onClose = () => {
+        setOpen(false);
+    }
+    
     useEffect(() => {
         getArtworks().then(({data}) => {
             setArtworkData(data);
@@ -25,13 +33,34 @@ export function HomePage() {
     return (
         <div className="m-2">
             <h1 className="text-center">Welcome to the home page!</h1>
-        <div className="image-border mr-auto ml-auto w-full rounded-lg mt-10 text-center">
+        <div className="image-border mr-auto ml-auto w-full max-w-2xl rounded-lg mt-10 text-center">
             <Image height={350} src={artworkData[count] ? artworkData[count].images.web.url : <h1>Loading...</h1> } className="max-w-full object-contain flex mr-auto ml-auto"></Image>
         </div>
-        <div className="text-center m-5">
-            <Button icon={<LeftCircleOutlined className="text-xl"/>} onClick={()=> incrementCount(-1)}> </Button>
-            <Button icon={<RightCircleOutlined className="text-xl"/>} onClick={()=> incrementCount(1)}> </Button>
+        <div className="m-5 max-w-30 flex ml-auto mr-auto justify-center">
+            <Button icon={<LeftCircleOutlined className="text-2xl"/>} onClick={()=> incrementCount(-1)}> </Button>
+            <Button icon={<InfoCircleOutlined className="text-xl"/>} onClick={showDrawer} className="ml-5 mr-5"></Button>
+            <Button icon={<RightCircleOutlined className="text-2xl"/>} onClick={()=> incrementCount(1)}> </Button>
         </div>
+        <Drawer onClose={onClose} open={open} size="default" extra={
+            <Space>
+                <Button onClick={onClose}>OK</Button>
+            </Space>
+        }>
+            {artworkData[count] ? <>
+            <p className="text-2xl sm:text-4xl font-bold underline mb-1">{artworkData[count].title}</p>
+            <p className="text-lg mb-1">{artworkData[count].creation_date}</p>
+            <p className="underline">{artworkData[count].creators.length === 0 ? "No Artists Found" : artworkData[count].creators[0].description}</p>
+            <p className="mb-1">{artworkData[count].culture[0]}</p> 
+            <p>{artworkData[count].technique}</p>
+            <Divider>Description</Divider>
+            <p className="indent-1 first-letter:text-6xl first-letter:font-bold first-letter:me-2 first-letter:float-start">{artworkData[count].description}</p>
+            <Divider>Current Location</Divider>
+            <p>{artworkData[count].current_location}</p>
+            <Divider>Did you know ?</Divider>
+            <p>{artworkData[count].did_you_know}</p>
+            </>
+            : <h1>Loading...</h1> }
+        </Drawer>
         </div>
     )
 }
