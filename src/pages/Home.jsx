9a38a873a8981a027c8152db00e6aa4b3860c379
ronaldb123 <ronaldbeck@hotmail.co.uk>
2024-03-27 14:34:17 +0000

    fixed image resolution issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { getArtworks } from "../utility/api"
 import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons"
-import { Button } from "antd";
+import { Button, Image } from "antd";
 
 
 export function HomePage() {
@@ -25,8 +25,8 @@ export function HomePage() {
     return (
         <div className="m-2">
             <h1 className="text-center">Welcome to the home page!</h1>
-        <div className="image-border mr-auto ml-auto w-full h max-w-xs max-h-xs rounded-lg mt-10 text-center">
-            <img src={artworkData[count] ? artworkData[count].images.web.url : <h1>Loading...</h1> } className="max-w-full h-25 object-contain flex mr-auto ml-auto"></img>
+        <div className="image-border mr-auto ml-auto w-full rounded-lg mt-10 text-center">
+            <Image height={350} src={artworkData[count] ? artworkData[count].images.web.url : <h1>Loading...</h1> } className="max-w-full object-contain flex mr-auto ml-auto"></Image>
         </div>
         <div className="text-center m-5">
             <Button icon={<LeftCircleOutlined className="text-xl"/>} onClick={()=> incrementCount(-1)}> </Button>
