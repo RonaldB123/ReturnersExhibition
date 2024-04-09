f2e0a5f46d2aab7a05dc01aa57487004a5e3c0b8
ronaldb123 <ronaldbeck@hotmail.co.uk>
2024-04-09 14:19:26 +0100

    refactored files and added sculptures page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { HomePage } from "./pages/Home"
 import { useEffect, useState } from "react"
 import { Menu } from "./components/Menu";
 import { FavouritesPage } from "./pages/Favourites";
+import { PaintingsPage } from "./pages/Paintings";
+import { Sculptures } from "./pages/Sculptures";
 
 function App() {
   const [favArt, setFavArt] = useState([]);
@@ -23,8 +25,10 @@ function App() {
     <>
     <Menu favArt={favArt}/>
       <Routes>
-        <Route path="/" element={<HomePage setFavArt={setFavArt} favArt={favArt}/>}/>
-        <Route path="/favourites" element={<FavouritesPage setFavArt={setFavArt} favArt={favArt}/>}/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/Favourites" element={<FavouritesPage setFavArt={setFavArt} favArt={favArt}/>}/>
+        <Route path="/Paintings" element={<PaintingsPage setFavArt={setFavArt} favArt={favArt}/>} />
+        <Route path="/Sculptures" element={<Sculptures/>}/>
       </Routes>
     </>
   )
---
+++ src/pages/Paintings.jsx
@@ -0,0 +1,152 @@
+import { useEffect, useState } from "react"
+import { getArtworks, getSearchedArtworks } from "../utility/api"
+import { InfoCircleOutlined, LeftCircleOutlined, LoadingOutlined, PictureOutlined, RightCircleOutlined, RollbackOutlined, StarFilled, StarOutlined } from "@ant-design/icons"
+import { Button, Divider, Drawer, Empty, Image, Row, Space, Spin } from "antd";
+import { SearchBar } from "../components/SearchArt";
+
+
+export function PaintingsPage({setFavArt, favArt}) {
+    const [artworkData, setArtworkData] = useState([]); 
+    const [count, setCount] = useState(0);
+    const [open, setOpen] = useState(false);
+    const [keySearch, setKeySearch] = useState('');
+    const [fav, setFav] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    const incrementCount = (increment) => {
+        if((increment === -1 && count === 0) || (increment === 1 && count === artworkData.length -1)){
+
+        }else{
+            setCount((currentCount) => currentCount + increment);
+        }
+    }
+    const showDrawer = () => {
+        setOpen(true);
+    }
+    
+    const onClose = () => {
+        setOpen(false);
+    }
+
+    const addToFav = (obj) => {
+        setFavArt((currentArr)=>{
+            return currentArr.concat(obj);
+        });
+    }
+
+    const removeFromFav = (objId) => {
+        setFavArt((currentArr)=>{
+            return currentArr.filter(obj=>obj.id !== objId)
+        })
+    }
+
+    useEffect(() => {
+        if(keySearch === ''){
+            getArtworks().then(({data}) => {
+                setArtworkData(data);
+                setLoading(false);
+            }).catch((err)=> console.log(err));
+        }else{
+            getSearchedArtworks(keySearch).then(({data}) => {
+                setArtworkData(data);
+                setLoading(false);
+            }).catch((err) => console.log(err));
+        }
+    },[keySearch])
+    return (
+        <>
+        {/* Main Image and Image-Border */}
+        <div className="m-2 max-w-full sm:float-left">
+            <h1 className="text-center">Explore Paintings!</h1>
+        <div className="sm:mr-auto sm:ml-auto image-border max-w-2xl rounded-lg mt-10 text-center sm:hidden">
+        <Spin spinning={loading}>
+            <Image height={350} src={artworkData.length > 0 ? artworkData[count].images.web.url : <Spin/>} className="object-contain flex mr-auto ml-auto max-w-2xl"></Image>
+        </Spin>
+        </div>
+        <div className="sm:mr-auto sm:ml-auto image-border max-w-2xl rounded-lg mt-10 text-center hidden sm:flex">
+        <Spin spinning={loading}>
+            <Image height={350} width={500} src={artworkData.length > 0 ? artworkData[count].images.web.url : <Spin/>} className="object-contain flex mr-auto ml-auto max-w-2xl"></Image>
+        </Spin>
+        </div>
+        
+        {/* Image buttons */}
+        <Row justify={"space-between"} className="mt-2 mb-2 max-w-xs mr-auto ml-auto">
+            <Row span={4}>
+            {keySearch
+            ?<Button icon={<RollbackOutlined className="text-xl"/>} onClick={()=>{
+                setKeySearch('');
+                setCount(0);
+                setLoading(true);
+            }}></Button> 
+            : <Button icon={<RollbackOutlined className="text-xl"/>} className="invisible"></Button>
+            }
+            </Row>
+
+            <Row span={4}>
+            <Button icon={<LeftCircleOutlined className="text-2xl"/>} onClick={()=>{
+                incrementCount(-1);
+                if(artworkData[count-1]){
+                    if(favArt.some(obj=> obj.id === artworkData[count-1].id)){
+                        setFav(true);
+                    }else{
+                        setFav(false);
+                    }
+                }
+             }}> </Button>
+            <Button icon={<InfoCircleOutlined className="text-xl"/>} onClick={showDrawer} className="ml-5 mr-5 md:invisible"></Button>
+            <Button icon={<RightCircleOutlined className="text-2xl"/>} onClick={()=>{
+                incrementCount(1);
+                if(artworkData[count+1]){
+                    if(favArt.some(obj=> obj.id === artworkData[count+1].id)){
+                        setFav(true);
+                    }else{
+                        setFav(false);
+                    }
+                }
+                }}> </Button>
+            </Row>
+            <Row span={4}>
+                {fav 
+                ?<Button onClick={()=>{
+                    setFav(false);
+                    removeFromFav(artworkData[count].id);
+                }} icon={<StarFilled  className="text-xl" />}></Button> 
+                :<Button onClick={()=>{
+                    setFav(true);
+                    addToFav(artworkData[count]);
+                }} icon={<StarOutlined  className="text-xl" />}></Button>
+                }
+            </Row>
+        </Row>
+        
+        {/* Information Drawer and Button */}
+        <Drawer onClose={onClose} open={open} size="default" extra={
+            <Space>
+                <Button onClick={onClose}>OK</Button>
+            </Space>
+        }>
+            {/* Drawer artwork information */}
+            {artworkData.length >= 1? <>
+            <p className="text-2xl sm:text-4xl font-bold underline mb-1">{artworkData[count].title}</p>
+            <p className="text-lg mb-1">{artworkData[count].creation_date}</p>
+            <p className="underline">{artworkData[count].creators.length === 0 ? "No Artists Found" : artworkData[count].creators[0].description}</p>
+            <p className="mb-1">{artworkData[count].culture[0]}</p> 
+            <p>{artworkData[count].technique}</p>
+            <Divider>Description</Divider>
+            <p className="indent-1 first-letter:text-6xl first-letter:font-bold first-letter:me-2 first-letter:float-start">{artworkData[count].description}</p>
+            <Divider>Current Location</Divider>
+            <p>{artworkData[count].current_location}</p>
+            <Divider>Did you know ?</Divider>
+            <p>{artworkData[count].did_you_know}</p>
+            </>
+            : <Empty/> }
+        </Drawer>
+        
+        {/* Search bar  */}
+        <div className="max-w-xs ml-auto mr-auto">
+            <SearchBar setKeySearch={setKeySearch}/>
+        </div>
+        </div>
+        </>
+    )
+}
