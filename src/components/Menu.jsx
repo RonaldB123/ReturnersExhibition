e2c7e6900cc47ebb3e1ec4865c3437ee21fd0e62
ronaldb123 <ronaldbeck@hotmail.co.uk>
2024-04-21 23:47:11 +0100

    fixed menu link issue

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,13 @@
 import { CommentOutlined, HomeFilled, MenuOutlined, PictureFilled, StarFilled, TrophyFilled } from "@ant-design/icons"
 import { FloatButton } from "antd"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 
 
 export const Menu = ({ favArt }) => {
+
+    const navigate = useNavigate();
+
     return (
         <>
                 <FloatButton.Group
@@ -16,10 +19,10 @@ export const Menu = ({ favArt }) => {
                     badge={{ count: favArt.length || 0 }}
                     icon={<MenuOutlined />}
                 >
-                    <FloatButton icon={<Link to={"/paintings"}><PictureFilled /></Link>} tooltip={"Paintings"} />
-                    <FloatButton icon={<Link to={"/sculptures"}><TrophyFilled /></Link>} tooltip={"Antiques/Sculptures"} />
-                    <FloatButton icon={<Link to={"/favourites"}><StarFilled /></Link>} badge={{ count: favArt.length }} tooltip={"Favourites"} />
-                    <FloatButton icon={<Link to={"/"}><HomeFilled /></Link>} tooltip={"Home"} />
+                    <FloatButton onClick={()=> navigate("/paintings")} icon={<PictureFilled />} tooltip={"Paintings"} />
+                    <FloatButton onClick={()=> navigate("/sculptures")} icon={<TrophyFilled />} tooltip={"Antiques/Sculptures"} />
+                    <FloatButton onClick={()=> navigate("/favourites")} icon={<StarFilled />} badge={{ count: favArt.length }} tooltip={"Favourites"} />
+                    <FloatButton onClick={()=> navigate("/")} icon={<HomeFilled />} tooltip={"Home"} />
                 </FloatButton.Group>
         </>
     )
